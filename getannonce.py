6e8079dfcda8c1cbf22965af8d3223d577ad1505
doraemonext <doraemonext@gmail.com>
2014-08-30 08:54:07 +0800

    Refactory: improved sendmail function

--- getannonce.py
+++ getannonce.py
@@ -4,15 +4,20 @@
 import urllib2 
 import os
 import sys   
-from threading import Timer 
-from bs4 import BeautifulSoup
 import re
 import smtplib
+import mimetypes
 import base64
+from threading import Timer
+from bs4 import BeautifulSoup
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.mime.image import MIMEImage
 
 
-reload(sys)  
-sys.setdefaultencoding('utf-8')  
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 f = open("urls.txt", "w+")
 rawurls = f.readlines()  # 分行读取文件中的链接并存入列表（链接后有换行符）
 urls = []
@@ -23,19 +28,25 @@ for url in rawurls:
     urls.append(url)  # 剪切换行符并把 url 存入 urls 列表
 
 
-def sendemail(msg):
+def sendemail(subject, content):
     """
     发送邮件函数
-    :param msg: 邮件标题内容
+    :param subject: 邮件标题内容
+    :param content: 邮件正文内容
+    :return:
     """
     username = "monitor@ziqiang.net"
-    # 密码
     password = "ziqiang@monitor"
-    # smtp会话过程中的mail from地址
-    from_addr = "monitor@ziqiang.net"
-    # smtp会话过程中的rcpt to地址
-    # to_addr = "mobile@ziqiang.net"
-    to_addr = "963949236@qq.com"
+
+    sender = "monitor@ziqiang.net"
+    recipients = ["mobile@ziqiang.net", "963949236@qq.com"]
+
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = ", ".join(recipients)
+    message["Subject"] = Header(subject, charset="UTF-8")
+    message.attach(MIMEText(content, _subtype="plain", _charset="UTF-8"))
+
     svr = smtplib.SMTP()
     svr.connect('smtp.gmail.com:25')
     # 设置为调试模式，就是在会话过程中会有输出信息
@@ -45,8 +56,8 @@ def sendemail(msg):
     svr.starttls()  # <------ 这行就是新加的支持安全邮件的代码！
     # 发送正文数据
     svr.login(username, password)
-    svr.sendmail(from_addr, to_addr, 'Subject:'+msg)
-    print "发送成功"
+    svr.sendmail(sender, recipients, message.as_string())
+    print u"发送成功"
     # 发送结束，退出
     svr.quit()
 
@@ -78,10 +89,10 @@ def getannonce(name, url_path, comth, comstr, headurl):
                 f.write("\n")  # 加换行符分行
                 url_ = headurl+url
                 if i.strong:  # 有些公告是加粗字体
-                    mailstr = "【"+name+"】有新公告！>>>>"+i.strong.string+"\n\n"+url_  # mailstr 中的两个换行符使 url_ 变成正文部分
+                    mailstr = u"【"+name+u"】有新公告！>>>>"+i.strong.string+"\n\n"+url_  # mailstr 中的两个换行符使 url_ 变成正文部分
                     sendemail(mailstr)
                 else:
-                    mailstr = "【"+name+"】有新公告！>>>>"+i.string+"\n\n"+url_
+                    mailstr = u"【"+name+u"】有新公告！>>>>"+i.string+"\n\n"+url_
                     sendemail(mailstr)
 
 
@@ -90,36 +101,36 @@ def getall():
     获取所有公告信息
     """
     # 官网主页
-    getannonce("武大官网", "http://www.whu.edu.cn/tzgg.htm", "href", "info", "http://www.whu.edu.cn/")
+    getannonce(u"武大官网", "http://www.whu.edu.cn/tzgg.htm", "href", "info", "http://www.whu.edu.cn/")
     # 电影消息
-    getannonce("电影消息", "http://vhost.whu.edu.cn/gh/xywh.php?Class_Type=0&Class_ID=42", "href", "xywh_Cons.php", "http://vhost.whu.edu.cn/gh/")
+    getannonce(u"电影消息", "http://vhost.whu.edu.cn/gh/xywh.php?Class_Type=0&Class_ID=42", "href", "xywh_Cons.php", "http://vhost.whu.edu.cn/gh/")
     # 教务部
-    getannonce("教务部", "http://jwb.whu.edu.cn/infocenter.aspx", "href", "Archives", "http://jwb.whu.edu.cn/")
+    getannonce(u"教务部", "http://jwb.whu.edu.cn/infocenter.aspx", "href", "Archives", "http://jwb.whu.edu.cn/")
     # 保卫部
-    getannonce("保卫部", "http://sub.whu.edu.cn/protect/gzdt/", "class", "url", "http://sub.whu.edu.cn/protect/")
+    getannonce(u"保卫部", "http://sub.whu.edu.cn/protect/gzdt/", "class", "url", "http://sub.whu.edu.cn/protect/")
     # 考试中心
-    getannonce("考试中心", "http://exam.whu.edu.cn/News/zhks/", "href", "^/News/k", "http://exam.whu.edu.cn")
+    getannonce(u"考试中心", "http://exam.whu.edu.cn/News/zhks/", "href", "^/News/k", "http://exam.whu.edu.cn")
     # 一站式
-    getannonce("一站式", "http://yzs.whu.edu.cn/article/?s=%E4%BF%A1%E6%81%AF%E5%8F%91%E5%B8%83", "href", "yzs.whu.edu.cn/article", "")
+    getannonce(u"一站式", "http://yzs.whu.edu.cn/article/?s=%E4%BF%A1%E6%81%AF%E5%8F%91%E5%B8%83", "href", "yzs.whu.edu.cn/article", "")
     # 学工部
-    getannonce("学工部", "http://xgb.whu.edu.cn/list.asp?id=45", "href", "Article.asp", "http://xgb.whu.edu.cn/")
+    getannonce(u"学工部", "http://xgb.whu.edu.cn/list.asp?id=45", "href", "Article.asp", "http://xgb.whu.edu.cn/")
     # 校医院
-    getannonce("校医院", "http://hospital.whu.edu.cn/tiltelist.aspx?Newstype=1", "href", "content.aspx", "http://hospital.whu.edu.cn/")
+    getannonce(u"校医院", "http://hospital.whu.edu.cn/tiltelist.aspx?Newstype=1", "href", "content.aspx", "http://hospital.whu.edu.cn/")
     # 武装部 有两个页面
-    getannonce("武装部", "http://rwb.whu.edu.cn/list.asp?id=49", "href", "Article.asp", "http://rwb.whu.edu.cn/")
-    getannonce("武装部", "http://rwb.whu.edu.cn/list.asp?id=45", "href", "Article.asp", "http://rwb.whu.edu.cn/")
+    getannonce(u"武装部", "http://rwb.whu.edu.cn/list.asp?id=49", "href", "Article.asp", "http://rwb.whu.edu.cn/")
+    getannonce(u"武装部", "http://rwb.whu.edu.cn/list.asp?id=45", "href", "Article.asp", "http://rwb.whu.edu.cn/")
     # 网络中心
-    getannonce("网络中心", "http://nic.whu.edu.cn/", "href", "m=announce", "")
+    getannonce(u"网络中心", "http://nic.whu.edu.cn/", "href", "m=announce", "")
     # 财务部
-    getannonce("财务部", "http://finance.whu.edu.cn/finance/wenjiantongzhi/wjtz.htm", "href", "WJTZ", "http://finance.whu.edu.cn/finance/wenjiantongzhi/")
+    getannonce(u"财务部", "http://finance.whu.edu.cn/finance/wenjiantongzhi/wjtz.htm", "href", "WJTZ", "http://finance.whu.edu.cn/finance/wenjiantongzhi/")
     # 国交中心
-    getannonce("国交中心", "http://oir.whu.edu.cn/list/?33_1.html", "href", "content/", "http://oir.whu.edu.cn/")
+    getannonce(u"国交中心", "http://oir.whu.edu.cn/list/?33_1.html", "href", "content/", "http://oir.whu.edu.cn/")
     # 就业指导中心 列表中包括下一页，尾页链接（index)
-    getannonce("就业指导中心", "http://www.xsjy.whu.edu.cn/zxtg//", "href", "zxtg/", "http://www.xsjy.whu.edu.cn/")
+    getannonce(u"就业指导中心", "http://www.xsjy.whu.edu.cn/zxtg//", "href", "zxtg/", "http://www.xsjy.whu.edu.cn/")
     # 实验部 中文乱码，在函数中未添加转码的部分（fromEncoding="gb18030"）
-    getannonce("实验部", "http://lab.whu.edu.cn/tzgg/", "href", "tzgg/", "http://lab.whu.edu.cn/")
+    getannonce(u"实验部", "http://lab.whu.edu.cn/tzgg/", "href", "tzgg/", "http://lab.whu.edu.cn/")
     # 后勤部
-    getannonce("后勤部", "http://hqbzb.whu.edu.cn/list.aspx?id=28", "href", "ShowArticle.aspx", "http://hqbzb.whu.edu.cn/")
+    getannonce(u"后勤部", "http://hqbzb.whu.edu.cn/list.aspx?id=28", "href", "ShowArticle.aspx", "http://hqbzb.whu.edu.cn/")
 
 print getall()
 #循环部分
